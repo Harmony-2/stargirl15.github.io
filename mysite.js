c82d422592555873890fbfb3c963956e69b44877
Lia <liaharmony@gmail.com>
2019-07-09 15:26:59 -0700

    initial commit

---
+++ mysite.js
@@ -0,0 +1,11 @@
+alert("welcome to my site!");
+
+function bigImg(x){
+  x.style.height = "518px";
+  x.style.width = "390px";
+}
+
+function smallImg(x){
+  x.style.height = "259px";
+  x.style.width = "195px";
+}
